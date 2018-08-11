4eae8bd89ebad4713948f1d9e92fde8ac96b69f4
octocatz <lanzartista@gmail.com>
2018-08-11 08:40:58 +0000

    web developed

--- index.php
+++ index.php
@@ -1,32 +1,25 @@
 <!DOCTYPE html>
 <html>
-	<head>
+<head>
 <meta charset="utf-8">
 <title>POST_SAMPLE</title>
 <link rel="stylesheet" type="text/css" href="style.css">
-	</head>
-	<body>
-<h1>Tensorflowでネコあてゲーム</h1>
-		<h2>判別したいネコさんを選択して[Check]をおしてね。</h2><p>
-		<form name="tfaction" action="tf.php" method="POST">
-	<p>ネコ1<input type="radio" name="q1" value="cat1" checked><p><img src="./img/cat1.jpg" class="main_img">
-	<p>ネコ2<input type="radio" name="q1" value="cat2"><p><img src="./img/cat2.jpg" class="main_img">
-	<p>ネコ3<input type="radio" name="q1" value="cat3"><p><img src="./img/cat3.jpg" class="main_img">
-<p>
-
-
-		<?php
-#var_dump( exec('ls',$out,$ret) );
-#print_r($out);
-#var_dump( $ret );
-#echo "#";
-#var_dump( exec('./start.sh',$out2,$ret2) );
-#print_r($out2);
-#var_dump( $ret2 );
-		?>
-			<p><input type="submit" name="check" value="Check" /></p>
-		</form>
-
-
-	</body>
+</head>
+<body class='stripe_base'>
+<h1>Tensorflowでねこの画像認識</h1>
+<h2>判別したいねこを選択して[Check]をおしてね。</h2><p>
+<form name="tfaction" action="tf.php" method="POST" enctype="multipart/form-data">
+<!--
+ <p>ネコ1<input type="radio" name="q1" value="cat1" checked><p><img src="./img/cat1.jpg" class="main_img">
+ <p>ネコ2<input type="radio" name="q1" value="cat2"><p><img src="./img/cat2.jpg" class="main_img">
+ <p>ネコ3<input type="radio" name="q1" value="cat3"><p><img src="./img/cat3.jpg" class="main_img">
+ <p>
+-->
+ ファイルをアップする:<input type="file" name="up_file"><br>
+ <input type="submit" value="アップしたファイルでCheck！">
+<!--
+<p><input type="submit" name="check" value="Checkする" /></p>
+-->
+</form>
+</body>
 </html>
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 imgname=$1
-imgfile="/home/redluckly/tf/$imgname"
+imgfile="$imgname"
 . /home/redluckly/tensorflow/venv/bin/activate && python3 /home/redluckly/tf/label_image.py --image $imgfile --graph /home/redluckly/tf/retrained_graph.pb --labels /home/redluckly/tf/retrained_labels.txt --input_layer=Placeholder > ./tf.log && deactivate
 
 res=`tail -n 5 ./tf.log`
